d14eea1782cf4e1fdeba6b5db07ce371c3a07200
Luis Santos <luis.correa@iteris.com.br>
2020-08-21 11:28:46 -0300

    First Commit - Project created on Spring Initializr

---
+++ src/main/java/br/com/luisergio/bomberarena/BomberarenaApplication.java
@@ -0,0 +1,13 @@
+package br.com.luisergio.bomberarena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomberarenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomberarenaApplication.class, args);
+	}
+
+}
